be89fe58a8b27bed0622fa61ddfe8ff0f0f9b809
Vikrant SIngh <vikrant.mnnit00@gmail.com>
2018-11-14 20:34:00 +0530

    Update timetable.php

---
+++ timetable.php
@@ -0,0 +1,90 @@
+<html>
+<head><title>Time table</title>
+	<link rel="stylesheet" href="assets/css/main.css" /></head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+
+
+<body>
+
+<div id="wrapper">
+
+        <header id="header">
+          <h1>Time Table</h1>
+          <p></p>
+        </header>
+
+        <div id="main">
+
+          <nav id="nav">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#"  class="active" >Time Table</a></li>
+            <li><a href="" >Log out</a></li>
+
+          </ul>
+        </nav>
+
+          <!-- Content -->
+            <section id="content" class="main">
+
+
+<!-- content write here -->
+<select name="class" id="class">
+                              <option value=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">- Select Class -</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Manufacturing</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Shipping</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Administration</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">human Resources</font></font></option>
+                            </select>
+
+
+<select name="sem" id="sem">
+                              <option value=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">- Select Class -</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Manufacturing</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Shipping</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Administration</font></font></option>
+                              <option value="1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">human Resources</font></font></option>
+                            </select>
+
+            </section>
+
+        </div>
+
+      <footer id="footer">
+            <section>
+              <h2>About Leave hive</h2>
+              <p>Leave hive is a genral web application for Approving leave online.</p>
+              <ul class="actions">
+                <li><a href="about.html" class="button">Learn More</a></li>
+              </ul>
+            </section>
+            <section>
+              <h2>Contact Us</h2>
+              <dl class="alt">
+
+                <dt>Phone</dt>
+                <dd>(+91)8090 410 264</dd>
+                <dt>Email</dt>
+                <dd><a href="mail.google.com/">Vikrant.mnnit00@gmail.com</a></dd>
+              </dl>
+              <ul class="icons">
+                <li><a href="https://twitter.com/svicky516" target="_blank" class="icon fa-twitter alt"><span class="label">Twitter</span></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100003661545540&ref=bookmarks" target="_blank" class="icon fa-facebook alt"><span class="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/crise_time/" target="_blank" class="icon fa-instagram alt"><span class="label">Instagram</span></a></li>
+                <li><a href="https://github.com/crisetime"  target="_blank" class="icon fa-github alt"><span class="label">GitHub</span></a></li>
+
+              </ul>
+            </section>
+            <p class="copyright">&copy; Leave hive team. Designd by Team hive/p>
+          </footer>
+
+  </div>
+
+
+</body>
+
+
+
+
+</html>
